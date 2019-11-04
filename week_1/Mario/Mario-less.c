8d870b50f834b516764b92eac21bf894f724e2c1
Joffrey Nolting <joffrey.nolting@gmail.com>
2019-11-04 12:51:11 +0100

    Create Mario-less.c

---
+++ week_1/Mario/Mario-less.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// function to get a number from user between 2 numbers
+int get_right_int(string prompt);
+
+int main(void)
+{
+    int height = get_right_int("Height: ");
+    //starting at 0, for each number in "height"...
+    for (int i = 0; i < height; i++)
+    {
+        printf("%*s", height - i - 1, ""); // trick to save a for loop
+        
+        for (int j = 0; j <= i; j+y+)
+        {
+            printf("#");       
+        }
+       
+        printf("\n");
+    }
+    
+}
+
+int get_right_int(string prompt)
+{
+    int n;
+    do
+    {
+        n = get_int("%s", prompt);
+    }
+    while (n < 1 || n > 8);
+    return n;
+}
