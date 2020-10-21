7d57cb01daea4fa0e3018f4592c3533b82e10274
Joffrey Nolting <joffrey.nolting@gmail.com>
2020-10-21 07:48:05 +0200

    Update ceasar.c
    ads error handling with command line arguments

--- pset2/ceasar.c
+++ pset2/ceasar.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <cs50.h>
 
+int main( int argc, char *argv[])
+{
+    printf("Number of arguments %i\n", argc);
+    printf("Given argument:  %s\n", argv[1]);
+    if(argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+}
