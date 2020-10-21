6d3f2066cd8f890cbfa2140968ae905a0a95cbfb
Joffrey Nolting <joffrey.nolting@gmail.com>
2020-10-21 08:35:01 +0200

    ads encryption

--- pset2/ceasar.c
+++ pset2/ceasar.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <cs50.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main( int argc, char *argv[])
 {
-    printf("Number of arguments %i\n", argc);
-    printf("Given argument:  %s\n", argv[1]);
     if(argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    int key = atoi(argv[1]);
+    printf("%i\n", key);
+    string  plainText = get_string("Plaintext: ");
+    printf("Cypertext: ");
+    for(int i = 0, n = strlen(plainText); i < n; i++)
+    {
+        printf("%c", (plainText[i] + key));
+    }
+    printf("\n");
+
 }
