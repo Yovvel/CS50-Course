57f53e47938207c095a13bac3097082781b78da9
Joffrey Nolting <joffrey.nolting@gmail.com>
2020-10-21 10:58:26 +0200

    Update ceasar.c
    Complete working program.

--- pset2/ceasar.c
+++ pset2/ceasar.c
@@ -6,19 +6,38 @@
 
 int main( int argc, char *argv[])
 {
-    if(argc != 2)
+    if(argc != 2 || !isdigit(argv[1][0]))
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    int key = atoi(argv[1]);
-    printf("%i\n", key);
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (isdigit(argv[1][i]) == 0)
+    {
+        return 1;
+    }
+    }
+     int key = atoi(argv[1]);
     string  plainText = get_string("Plaintext: ");
-    printf("Cypertext: ");
+
+    printf("ciphertext: ");
     for(int i = 0, n = strlen(plainText); i < n; i++)
     {
-        printf("%c", (plainText[i] + key));
+        if isupper(plainText[i])
+        {
+           printf("%c", (((plainText[i] + key)-65)%26)+65);
+        }
+        else if islower(plainText[i])
+        {
+           printf("%c", (((plainText[i] + key)-97)%26)+97);
+        }
+        else
+        {
+            printf("%c", plainText[i]);
+        }
+
     }
     printf("\n");
-
+    return 0;
 }
